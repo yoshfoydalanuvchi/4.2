2419350480f51e55e3cc335ae86f463b88774b1d
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-08 16:50:27 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,95 @@
+// 9-integer
+// let a = +prompt("3 xonali son kiriting");
+// let res = Math.trunc(a / 100);
+// alert(res);
+
+// 10-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// alert(ones + tens + faces);
+
+// 11-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// let faces = Math.trunc(a / 100);
+// alert(ones + tens + faces);
+
+// 12-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// let faces = Math.trunc(a / 100);
+// alert(ones * 100 + tens * 10 + faces);
+
+// 13-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// let faces = Math.trunc(a / 100);
+// alert(tens * 100 + ones * 10 + faces);
+
+// 14-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// let faces = Math.trunc(a / 100);
+// alert(ones * 100 + faces * 10 + tens);
+
+// 15-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// let faces = Math.trunc(a / 100);
+// alert(tens * 100 + faces * 10 + ones);
+
+// 16-integer
+// let a = +prompt("3 xonali son kiriting");
+// let ones = a % 10;
+// let tens = Math.trunc((a / 10) % 10);
+// let faces = Math.trunc(a / 100);
+// alert(faces * 100 + ones * 10 + tens);
+
+// 17-integer
+// let a = +prompt("4  xonali son kiriting");
+// let tens = Math.trunc((a / 10) % 10);
+// alert(tens);
+
+// 18-integer
+// let a = +prompt("4  xonali son kiriting");
+// let faces = Math.trunc((a / 100) % 10);
+// alert(faces);
+
+// 19-integer
+// let second = +prompt("sekundni kiriting");
+// let minute = Math.trunc(second / 60);
+// alert(minute);
+
+// 20-integer
+// let second = +prompt("sekundni kiriting");
+// let hour = Math.trunc(second / 3600);
+// alert(hour);
+
+// 21-integer
+// let second = +prompt("sekundni kiriting");
+// let seconds = second;
+// let minute = Math.trunc(second / 60);
+// alert("second " + second);
+// alert("minute " + minute);
+// 22-integer
+
+// let second = +prompt("sekundni kiriting");
+// let seconds = second;
+// let hour = Math.trunc(second / 3600);
+// alert("second " + second);
+// alert("hour " + hour);
+// 23-integer
+
+// let second = +prompt("sekundni kiriting");
+// let seconds = second;
+// let minute = Math.trunc(second / 60);
+// let hour = Math.trunc(second / 3600);
+// alert("hour " + hour);
+// alert("minute " + minute);
+// alert("second " + second);
